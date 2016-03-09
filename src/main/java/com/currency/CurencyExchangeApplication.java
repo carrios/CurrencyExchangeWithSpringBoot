d9f232baba556013a5d62edfd58192d684aa3390
carrios <carrios@o2.pl>
2016-03-09 20:56:03 +0100

    adding project from spring boot

---
+++ src/main/java/com/currency/CurencyExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurencyExchangeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/currency/CurencyExchangeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.currency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CurencyExchangeApplication.class)
+@WebAppConfiguration
+public class CurencyExchangeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
